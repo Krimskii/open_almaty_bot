f0f24ac5ae71ef3595a9d252a8a507e8b639bc6f
krimskii <Krimskii@hotmail.com>
2018-01-31 12:23:07 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-const TelegramBot = require('node-telegram-bot-api')
+/**
+ * This example demonstrates setting up webhook
+ * on the Heroku platform.
+ */
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = '514258171:AAHNdbKU9sz5KoB63afbIDNe-dltbkIc_ow';
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const TOKEN = process.env.TELEGRAM_TOKEN || '514258171:AAHNdbKU9sz5KoB63afbIDNe-dltbkIc_ow';
+const TelegramBot = require('../..');
+const options = {
+  webHook: {
+    // Port to which you should bind is assigned to $PORT variable
+    // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
+    port: process.env.PORT
+    // you do NOT need to set up certificates since Heroku provides
+    // the SSL certs already (https://<app-name>.herokuapp.com)
+    // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
+  }
+};
+// Heroku routes from port :443 to $PORT
+// Add URL of your app to env variable or enable Dyno Metadata
+// to get this automatically
+// See: https://devcenter.heroku.com/articles/dyno-metadata
+const url = process.env.APP_URL || 'https://telegram-krimskii.herokuapp.com:443';
+const bot = new TelegramBot(TOKEN, options);
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+// This informs the Telegram servers of the new webhook.
+// Note: we do not need to pass in the cert, as it already provided
+bot.setWebHook(`${url}/bot${TOKEN}`);
 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+// Just to ping!
+bot.on('message', function onMessage(msg) {
+  bot.sendMessage(msg.chat.id, 'I am alive on Heroku!');
 });
