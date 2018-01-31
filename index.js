097ac009a8c8ebb8204f4cad94eabdc2e87bbf6a
krimskii <Krimskii@hotmail.com>
2018-01-31 11:38:05 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '514258171:AAHGFn01xRUsBe9QzpuiQN8It74jMGeC0N0';
+const token = '514258171:AAHNdbKU9sz5KoB63afbIDNe-dltbkIc_ow';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
